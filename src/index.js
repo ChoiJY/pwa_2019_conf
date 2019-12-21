f5473cf785b74b64308ab3515aa4eafea9d0a486
Jun Young Choi <neocjy@naver.com>
2019-12-21 17:52:16 +0900

    parse error fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {PrimaryButton} from 'office-ui-fabric-react/lib/Button';
 
-const App = () => (
-    <div className="app"></div>
-);
+const App = () => {
+  <div className="app"></div>
+};
 
 ReactDOM.render(<App/>, document.getElementById('app'));
 
